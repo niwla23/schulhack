e9847cf9897cc33d3561fd43ef888a4f3ded0f7e
niwla23 <alwin@cloudserver.click>
2022-03-08 23:38:48 +0100

    fix naming

--- tools/renameAPKs.sh
+++ tools/renameAPKs.sh
@@ -1,2 +1,3 @@
-read -p "Version Name (format: major-minor-patch): " SCHULHACK_VERSION 
-rename app schulhack-$SCHULHACK_VERSION *.apk
+read -p "Version Name (format: major.minor.patch): " SCHULHACK_VERSION 
+rename app schulhack_v${SCHULHACK_VERSION} *.apk
+rename '-release' '_release' *.apk
